8e17fcef7a732bd8237fb21f10edaae7f817bca6
Diego Menin <dmenin@gmail.com>
2016-08-04 18:23:13 +0100

    .

--- 5_Capstone/DeepLearning/L1/softmax.py
+++ 5_Capstone/DeepLearning/L1/softmax.py
@@ -3,9 +3,9 @@ import numpy as np
 
 scores = np.array([3.0, 1.0, 0.2])
 x = [3.0, 1.0, 0.2]
-scores = np.array([[1, 2, 3, 6],
-                   [2, 4, 5, 6],
-                   [3, 8, 7, 6]])
+x = np.array([[1, 2, 3, 6],
+              [2, 4, 5, 6],
+              [3, 8, 7, 6]])
 
 
 
@@ -15,7 +15,7 @@ def softmax(x):
     return np.exp(x) / np.sum(np.exp(x), axis=0)
 
 
-print(softmax(scores/10  ))
+print(softmax(scores ))
 
 # Plot softmax curves
 import matplotlib.pyplot as plt
@@ -29,3 +29,18 @@ plt.show()
 
 x.shape
 scores.shape
+
+
+###################
+foo = 10e9
+bar = 10e-6
+for i in range(0,1000000):
+    foo +=  bar
+foo = foo - 10e9
+foo 
+
+
+a = 1000000000
+for i in xrange(1000000):
+    a +=  1e-6
+print a - 1000000000
